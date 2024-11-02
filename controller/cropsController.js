fa3d7670085b9daf5d5b60ba361e7f19e66c67ca
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-11-02 23:37:34 +0530

    edit button & delete button implement in crop table.

--- controller/cropsController.js
+++ controller/cropsController.js
@@ -15,3 +15,31 @@ $(document).ready(function () {
     $("#cropScientificName").val(scientificName);
   });
 });
+
+$(document).ready(function () {
+  $("#btn-edit-crop").on("click", function () {
+    const row = $(this).closest("tr");
+    const cropId = row.find("td:eq(0)").text();
+    const cropCommonName = row.find("td:eq(1)").text();
+    const cropScientificName = row.find("td:eq(2)").text();
+    const fieldType = row.find("td:eq(3)").text();    
+    const cropCategory = row.find("td:eq(4)").text();
+    const cropSeason = row.find("td:eq(5)").text();
+    const cropImg = row.find("td:eq(6)").text();
+
+    $("#editCropId").val(cropId);
+    $("#editFieldType").val(fieldType);
+    $("#editCropCommonName").val(cropCommonName);
+    $("#editCropScientificName").val(cropScientificName);
+    $("#editCropCategory").val(cropCategory);
+    $("#editCropSeason").val(cropSeason);
+    $("#editCropImg").val(cropImg);
+
+    $("#editCropModal").modal("show");
+  });
+
+  $("#editCropForm").on("submit", function (event) {
+    event.preventDefault();
+    $("#editCropModal").modal("hide");
+  });
+});
