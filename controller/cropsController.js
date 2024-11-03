0daa99125858240bc367afd0bad920d5ed9653fc
T.H. Sandun Dilshan <Sandundil2002@gmail.com>
2024-11-03 19:53:18 +0530

    changed event listener from #cropCommonName to .cropCommonName for increase code reusability.

--- controller/cropsController.js
+++ controller/cropsController.js
@@ -9,20 +9,18 @@ const cropScientificNames = {
 };
 
 $(document).ready(function () {
-  $("#cropCommonName").on("change", function () {
+  $(".cropCommonName").on("change", function () {
     const selectedCommonName = $(this).val();
     const scientificName = cropScientificNames[selectedCommonName];
-    $("#cropScientificName").val(scientificName);
+    $(".cropScientificName").val(scientificName);
   });
-});
 
-$(document).ready(function () {
   $("#btn-edit-crop").on("click", function () {
     const row = $(this).closest("tr");
     const cropId = row.find("td:eq(0)").text();
     const cropCommonName = row.find("td:eq(1)").text();
     const cropScientificName = row.find("td:eq(2)").text();
-    const fieldType = row.find("td:eq(3)").text();    
+    const fieldType = row.find("td:eq(3)").text();
     const cropCategory = row.find("td:eq(4)").text();
     const cropSeason = row.find("td:eq(5)").text();
     const cropImg = row.find("td:eq(6)").text();
@@ -36,6 +34,11 @@ $(document).ready(function () {
     $("#editCropImg").val(cropImg);
 
     $("#editCropModal").modal("show");
+  });
+
+  $(".dropdown-item").click(function () {
+    var selectedValue = $(this).data("value");
+    $("#dropdownMenuButton").text(selectedValue);
   });
 
   $("#editCropForm").on("submit", function (event) {
